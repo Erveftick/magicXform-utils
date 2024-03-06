b794c6e79c2fbfb2ddd91eb901ab58aaa672bb7e
Erveftick <edwardkvashin@gmail.com>
2024-03-06 16:38:04 -0500

    Experiment added

---
+++ experiments/snufkin_2/exp-template/run/run-crown-enigma-subs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+TOOL_NAME=crown
+OPTS_NAME=enigma
+TOOL=/home/ekvashyn/Code/mXf/experiments/snufkin_2/exp-template/run/crown-enigma.sh
+
+EXP_NAME=$(python -m spacer.exp_name /home/ekvashyn/Code/mXf/experiments/snufkin_2/exp-template/idx/benchmarks_subs.idx)
+
+OUT=/home/ekvashyn/Code/mXf/experiments/snufkin_2/exp-template/out/${TOOL_NAME}.${OPTS_NAME}.${EXP_NAME}
+
+GRN="$(tput setaf 2)"
+RST="$(tput sgr0)"
+
+echo -e "Experiment: ${GRN}${EXP_NAME}${RST}"
+echo "Creating output directory: $OUT"
+mkdir -p $OUT
+
+PARALLEL=/home/ekvashyn/Code/apps/parallel/bin/parallel
+JOBS=8
+
+echo "Starting GNU parallel with $JOBS jobs ..."
+time $PARALLEL -j $JOBS --ungroup --results $OUT/{/} $TOOL :::: /home/ekvashyn/Code/mXf/experiments/snufkin_2/exp-template/idx/benchmarks_subs.idx
